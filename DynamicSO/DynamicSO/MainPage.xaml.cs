e9eeabff98b2fb84a2f95ad880442bcd95b790ea
hobbelb <hobbelb@VT-LT-221>
2022-10-16 19:24:17 +0200

    Add project files.

---
+++ DynamicSO/DynamicSO/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace DynamicSO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Image1.Style = (Style)Resources["selectedStyle"];
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Image1.Style = (Style)Resources["defaultStyle"];
+        }
+    }
+}
